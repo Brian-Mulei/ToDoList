772f7e1ebff8b62f282cc4e6c3b23ea385cabfcf
Brian-Mulei <brianmulei0@gmail.com>
2023-12-05 23:46:11 +0300

    Login and Register Pages

--- ToDoList/Views/LogInView.swift
+++ ToDoList/Views/LogInView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct LogInView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView{
+            VStack{
+                //Header
+                HeaderView(title: "To Do List", subtitle: "Just Do It", angle: 15,
+                           background: .pink)
+                
+                //login Form
+                
+                Form{
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button{
+                        
+                    }label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            
+                            Text("Log In")
+                                .foregroundColor(.white).bold()
+                        }
+                    }.padding()
+                }
+                
+                VStack{
+                    Text("New around here?")
+                    
+                    NavigationLink("Create an Account", destination:RegisterView())
+                    
+                }.padding(.bottom, 20)
+                
+                Spacer()
+            }
+        }
+        
     }
 }
 
